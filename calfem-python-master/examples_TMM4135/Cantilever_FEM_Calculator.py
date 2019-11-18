056c9e523c453da98efb2b9c55438b2d2e7c1b83
Natanael Ferreira Hjermann <natanaeh@ntnu.no>
2019-11-18 14:34:40 +0100

    ferdig med alt!

--- calfem-python-master/examples_TMM4135/Cantilever_FEM_Calculator.py
+++ calfem-python-master/examples_TMM4135/Cantilever_FEM_Calculator.py
@@ -12,19 +12,19 @@ import calfem.vis as cfv
 
 #Element Type
 
-nu = 0.4
-E =5
-Dmat = np.mat([
-        [ 1.0,  nu,  0.],
-        [  nu, 1.0,  0.],
-        [  0.,  0., (1.0-nu)/2.0]]) * E/(1.0-nu**2)
+# nu = 0.4
+# E =5
+# Dmat = np.mat([
+#         [ 1.0,  nu,  0.],
+#         [  nu, 1.0,  0.],
+#         [  0.,  0., (1.0-nu)/2.0]]) * E/(1.0-nu**2)
+#
+# ex = np.array([0,0,2])
+# ey = np.array([1,-1,-1])
+#
+# val = tri.tri6_Kmatrix(ex,ey,Dmat, 0.5)
 
-ex = np.array([0,0,2])
-ey = np.array([1,-1,-1])
-
-val = tri.tri6_Kmatrix(ex,ey,Dmat, 0.5)
-
-numElementNodes = 6  # Valid numbers 3, 4, 6, 9
+numElementNodes = 9  # Valid numbers 3, 4, 6, 9
 
 elTypeInfo= [-1,'Unknown elementtype']
 
@@ -40,8 +40,8 @@ elif numElementNodes == 9:
 
 # Number of nodes: Should be odd numbers in order to handle
 
-numNodesX = 30
-numNodesY = 30
+numNodesX = 60
+numNodesY = 60
 
 # number of patches that will fit a 9 node element
 numPatchX = (numNodesX-1) // 2
@@ -67,10 +67,10 @@ L         = 10.0
 thickness =  1
 
 # Distributed load in x and y, load pr unit area
-eq = np.array([5e4,0])
+eq = np.array([0,0])
 #End load, Given as resultant
 
-endLoadXY = np.array([0.0,5e5])
+endLoadXY = np.array([10000.0,0.0])
 #endLoadXY = np.array([3.0e6,0])
 #endLoadXY = np.array([4.2e9,0.0]) # Should give unit disp at Poisson = 0
 
@@ -168,7 +168,7 @@ for iel in range(elnods.shape[0]):
 
 
 # Draw the mesh.
-if bDrawMesh:
+if not bDrawMesh:
     cfv.drawMesh(
         coords=coords,
         edof=eldofs,
--- calfem-python-master/examples_TMM4135/quads.py
+++ calfem-python-master/examples_TMM4135/quads.py
@@ -97,78 +97,112 @@ def quad4e(ex, ey, D, th, eq=None):
     if eq is None:
         return Ke
     else:
-        fe = np.array(np.zeros((8, 1)))
-
         fe = getFe(ex,ey,th,eq)
 
         return Ke, fe
 
 
-def getN(epsilon, eta):
-    N = np.array(np.zeros(4))
-    N[0] = 0.25 * (1 - epsilon) * (1 - eta)
-    N[1] = 0.25 * (1 + epsilon) * (1 - eta)
-    N[2] = 0.25 * (1 + epsilon) * (1 + eta)
-    N[3] = 0.25 * (1 - epsilon) * (1 + eta)
+def getN(epsilon, eta, nodes):
+    if nodes == 4:
+        N = np.array(np.zeros(4))
+        N[0] = 0.25 * (1 - epsilon) * (1 - eta)
+        N[1] = 0.25 * (1 + epsilon) * (1 - eta)
+        N[2] = 0.25 * (1 + epsilon) * (1 + eta)
+        N[3] = 0.25 * (1 - epsilon) * (1 + eta)
+    if nodes == 9:
+        N = np.array(np.zeros(9))
+        N[0] = (epsilon * (epsilon - 1) * eta *(eta -1))/4
+        N[1] = (epsilon * (epsilon + 1) * eta *(eta -1))/4
+        N[2] = (epsilon * (epsilon + 1) * eta *(eta +1))/4
+        N[3] = (epsilon * (epsilon - 1) * eta *(eta +1))/4
+        N[4] = ((epsilon +1)*(epsilon-1)*eta*(eta-1))/-2
+        N[5] = (epsilon * (epsilon + 1) * (eta + 1) * (eta - 1)) / -2
+        N[6] = ((epsilon +1)*(epsilon-1)*eta*(eta+1))/-2
+        N[7] = (epsilon * (epsilon - 1) * (eta + 1) * (eta - 1)) / -2
+        N[8] = (epsilon + 1) * (epsilon - 1) * (eta + 1) * (eta - 1)
 
     return N
 
 
-def getN_eta(epsilon):
-    N_eta = np.array(np.zeros(4))
-    N_eta[0] = -0.25 * (1 - epsilon)
-    N_eta[1] = -0.25 * (1 + epsilon)
-    N_eta[2] = 0.25 * (1 + epsilon)
-    N_eta[3] = 0.25 * (1 - epsilon)
+def getN_eta(epsilon, eta, nodes):
+    if nodes == 4:
+        N_eta = np.array(np.zeros(4))
+        N_eta[0] = -0.25 * (1 - epsilon)
+        N_eta[1] = -0.25 * (1 + epsilon)
+        N_eta[2] = 0.25 * (1 + epsilon)
+        N_eta[3] = 0.25 * (1 - epsilon)
+    if nodes == 9:
+        N_eta = np.array(np.zeros(9))
+        N_eta[0] = (epsilon * (epsilon - 1) * (2*eta - 1))/4
+        N_eta[1] = (epsilon * (epsilon + 1) * (2*eta - 1))/4
+        N_eta[2] = (epsilon * (epsilon + 1) * (2*eta + 1))/4
+        N_eta[3] = (epsilon * (epsilon - 1) * (2*eta + 1))/4
+        N_eta[4] = ((epsilon + 1)*(epsilon - 1)*(2*eta -1))/-2
+        N_eta[5] = ((epsilon + 1)*(epsilon)*(2*eta))/-2
+        N_eta[6] = ((epsilon + 1)*(epsilon - 1)*(2*eta +1))/-2
+        N_eta[7] = ((epsilon - 1)*(epsilon)*(2*eta))/-2
+        N_eta[8] = ((epsilon + 1)*(epsilon - 1)*(2*eta))
 
     return N_eta
 
 
-def getN_epsilon(eta):
-    N_epsilon = np.array(np.zeros(4))
-    N_epsilon[0] = -0.25 * (1 - eta)
-    N_epsilon[1] = 0.25 * (1 - eta)
-    N_epsilon[2] = 0.25 * (1 + eta)
-    N_epsilon[3] = -0.25 * (1 + eta)
+def getN_epsilon(epsilon, eta, nodes):
+    if nodes == 4:
+        N_epsilon = np.array(np.zeros(4))
+        N_epsilon[0] = -0.25 * (1 - eta)
+        N_epsilon[1] = 0.25 * (1 - eta)
+        N_epsilon[2] = 0.25 * (1 + eta)
+        N_epsilon[3] = -0.25 * (1 + eta)
+    if nodes == 9:
+        N_epsilon = np.array(np.zeros(9))
+        N_epsilon[0] = (eta * (eta - 1) * (2*epsilon -1))/4
+        N_epsilon[1] = (eta * (eta - 1) * (2*epsilon +1))/4
+        N_epsilon[2] = (eta * (eta + 1) * (2*epsilon +1))/4
+        N_epsilon[3] = (eta * (eta + 1) * (2*epsilon -1))/4
+        N_epsilon[4] = ((eta)*(eta-1)*(2*epsilon))/-2
+        N_epsilon[5] = ((eta + 1)*(eta - 1) * (2*epsilon + 1))/-2
+        N_epsilon[6] = ((eta)*(eta+1)*(2*epsilon))/-2
+        N_epsilon[7] = ((eta + 1)*(eta - 1) * (2*epsilon - 1))/-2
+        N_epsilon[8] = ((eta + 1)*(eta - 1)*(2*epsilon))
 
     return N_epsilon
 
 
-def gety_eta(ex, ey, epsilon):
-    N_eta = getN_eta(epsilon)
+def gety_eta(ex, ey, epsilon, eta,nodes):
+    N_eta = getN_eta(epsilon, eta, nodes)
     y_eta = 0.0
     for i in range(len(ey)):
         y_eta += N_eta[i] * ey[i]
     return y_eta
 
 
-def gety_epsilon(ex, ey, eta):
-    N_epsilon = getN_epsilon(eta)
+def gety_epsilon(ex, ey, epsilon, eta, nodes):
+    N_epsilon = getN_epsilon(epsilon, eta, nodes)
     y_epsilon = 0.0
     for i in range(len(ey)):
         y_epsilon += N_epsilon[i] * ey[i]
     return y_epsilon
 
 
-def getx_eta(ex, ey, epsilon):
-    N_eta = getN_eta(epsilon)
+def getx_eta(ex, ey, epsilon, eta, nodes):
+    N_eta = getN_eta(epsilon,eta, nodes)
     x_eta = 0.0
     for i in range(len(ex)):
         x_eta += N_eta[i] * ex[i]
     return x_eta
 
 
-def getx_epsilon(ex, ey, eta):
-    N_epsilon = getN_epsilon(eta)
+def getx_epsilon(ex, ey, epsilon, eta, nodes):
+    N_epsilon = getN_epsilon(epsilon, eta, nodes)
     x_epsilon = 0.0
     for i in range(len(ex)):
         x_epsilon += N_epsilon[i] * ex[i]
     return x_epsilon
 
 
-def getN_eta_epsilon_mat(epsilon, eta):
-    n_eta = getN_eta(epsilon)
-    n_epsilon = getN_epsilon(eta)
+def getN_eta_epsilon_mat(epsilon, eta, nodes):
+    n_eta = getN_eta(epsilon, eta, nodes)
+    n_epsilon = getN_epsilon(epsilon, eta, nodes)
     mat = np.array([n_epsilon, n_eta])
     return mat
 
@@ -176,28 +210,30 @@ def getN_eta_epsilon_mat(epsilon, eta):
 def getB(ex, ey, epsilon, eta):
     B = np.array(np.zeros((3, 2 * len(ex))))
     J = getJacobi(ex, ey, epsilon, eta)
-    Nvec = getN_eta_epsilon_mat(epsilon, eta)
+    Nvec = getN_eta_epsilon_mat(epsilon, eta, len(ex))
     Jinv = np.linalg.inv(J)
     Nxyvec = Jinv @ Nvec
+    l = len(ex)
     for i in range(3):
         if i == 0:
-            for j in range(4):
+            for j in range(l):
                 B[i, 2 * j] = Nxyvec[0, j]
         if i == 1:
-            for j in range(4):
+            for j in range(l):
                 B[i, 2 * j + 1] = Nxyvec[1, j]
         if i == 2:
-            for j in range(4):
+            for j in range(l):
                 B[i, 2 * j] = Nxyvec[1, j]
                 B[i, 2 * j + 1] = Nxyvec[0, j]
     return B
 
 
 def getJacobi(ex, ey, epsilon, eta):
-    x_epsilon = getx_epsilon(ex, ey, eta)
-    x_eta = getx_eta(ex, ey, epsilon)
-    y_epsilon = gety_epsilon(ex, ey, eta)
-    y_eta = gety_eta(ex, ey, epsilon)
+    nodes = len(ex)
+    x_epsilon = getx_epsilon(ex, ey, epsilon, eta, nodes)
+    x_eta = getx_eta(ex, ey, epsilon, eta, nodes)
+    y_epsilon = gety_epsilon(ex, ey, epsilon, eta, nodes)
+    y_eta = gety_eta(ex, ey, epsilon, eta, nodes)
 
     jacobi = np.array([[x_epsilon, y_epsilon], [x_eta, y_eta]])
 
@@ -208,7 +244,8 @@ def getJacobi(ex, ey, epsilon, eta):
 
 
 def KeIntegral(ex, ey, D, th):
-    Ke = np.array(np.zeros((8, 8)))
+    l = len(ex)
+    Ke = np.array(np.zeros((l*2, l*2)))
     value = [0.0, 0.774597]
     weight = [0.88889, 0.555556]
 
@@ -232,7 +269,8 @@ def KeIntegral(ex, ey, D, th):
 def getFe(ex,ey,th,eq):
     value = [0.0, 0.774597]
     weight = [0.88889, 0.555556]
-    Fe = np.array(np.zeros((8,1)))
+    l = len(ex)
+    Fe = np.array(np.zeros((l*2,1)))
     q = np.array([eq])
     q = q.T
 
@@ -244,13 +282,17 @@ def getFe(ex,ey,th,eq):
 
     for i in range(len(value)):
         for j in range(len(value)):
+            N2 = np.array(np.zeros((2, l * 2)))
             J = getJacobi(ex, ey, value[i], value[j])
-            Nvec= getN(value[i], value[j])
-            N2 = np.array([[Nvec[0],0,Nvec[1],0,Nvec[2],0, Nvec[3],0],
-                           [0,Nvec[0],0,Nvec[1],0,Nvec[2],0, Nvec[3]]])
+            Nvec= getN(value[i], value[j],l)
+            for d in range(2):
+                for n in range(l):
+                    if d == 0:
+                        N2[d,2*n] = Nvec[n]
+                    if d == 1:
+                        N2[d, 2*n +1] = Nvec[n]
+
             N2 = N2.T
-
-
 
             Fe += N2 @ q * np.linalg.det(J) * weight[i] * weight[j] * th
 
@@ -293,14 +335,18 @@ def quad9e(ex, ey, D, th, eq=None):
     :return mat fe: consistent load vector [6 x 1] (if eq!=None)
     """
 
-    Ke = np.matrix(np.zeros((18, 18)))
-    fe = np.matrix(np.zeros((18, 1)))
-
     # TODO: fill out missing parts (or reformulate completely)
     #MUlig jeg kan kun endre N vektor og Nxy vektor til å tillate 9 noder også. Må huske at alle N'ene blir forskjellige ikke kun de nye
     #Tanken er at ved å bruke len(ex) eller noe lighnende kan de resterende funksjonene automatisk lage store nok matriser osv
 
+    Ke = KeIntegral(ex, ey, D, th)
+
+    # Bruk numerisk integrasjon
+    cyclic = [0, 1, 2, 3]
+
     if eq is None:
         return Ke
     else:
+        fe = getFe(ex, ey, th, eq)
+
         return Ke, fe
